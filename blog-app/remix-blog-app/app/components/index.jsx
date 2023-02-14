e76b610451cad1b60dfafaa25a93f3fc91688239
CamilingJS <jonathancamiling@gmail.com>
2023-02-13 22:08:39 -0800

    fulfilled all steps except last

---
+++ blog-app/remix-blog-app/app/components/index.jsx
@@ -0,0 +1,46 @@
+import BlogCard from "./BlogCard";
+import Layout from "./Layout";
+import Navbar from "./Navbar";
+export { BlogCard, Layout, Navbar };
+
+export const links = () => [
+  { rel: "stylesheet", href: styles },
+];
+
+export async function loader() {
+  checkEnvVars(); // check environmental variables
+  const response = await fetch(`${process.env.STRAPI_URL_BASE}/api/blogs?populate=hero`, {
+      method: "GET",
+      headers: {
+          "Authorization": `Bearer ${process.env.STRAPI_API_TOKEN}`,
+          "Content-Type": "application/json"
+      }
+  }); // get the blogs
+
+  checkStatus(response); // check the status
+
+  const data = await response.json(); // get the json response
+
+  if (data.error) { // error check
+      throw new Response("Error loading data from strapi", { status: 500 });
+  }
+
+  return data.data; // return the data
+}
+
+export default function Index() {
+  const blogs = useLoaderData();
+  return (
+      <Layout>
+          {
+              blogs.length > 0 ? (
+                  blogs.map(blog => (
+                      <BlogCard key={blog.id} blog={blog} />
+                  ))
+              ) : (
+                  <p>No blog posts found!</p>
+              )
+          }
+      </Layout>
+  );
+}
