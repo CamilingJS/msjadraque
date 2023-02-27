9d6bb28957849726b561714223c168698cc49f2e
CamilingJS <jonathancamiling@gmail.com>
2023-02-26 20:03:59 -0800

    updated footer

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 
 import './Footer.scss'
 
 const Footer = () => {
   return (
-    <div className='footer' >&copy; All rights reserved</div>
+    <div className='footer' >
+    <motion.div
+      whileInView={{ y: [100, 0], opacity: [0,1] }}
+      transition={{duration: 0.5}} 
+
+      id="contact"
+    >
+      <h4>Send any inquiries to:</h4>
+      <h4>info@jaycam.dev</h4>
+    </motion.div>
+    <h5>&copy; All rights reserved | Ms. Jadraque</h5> 
+    
+    </div>
   )
 }
 
